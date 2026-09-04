7c46224231fc74d4adbc4bbd89d7b4f565aee7fe
divakar singh purva <divakarpurva04@gmail.com>
2026-09-04 16:16:44 +0530

    fix: add the cron job function into cron route, and delete worker directory

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,15 +1,17 @@
+import { processScheduledPaymentsJob } from "@/jobs/payment/process-scheduled-payment";
+
 export function GET(request: Request) {
     const authHeader = request.headers.get('authorization');
     const cronSecret = process.env.CRON_SECRET;
 
-    console.log("Cron Entered")
+    console.log("CRON ENTERED=============================")
     if (!cronSecret || authHeader !== `Bearer ${cronSecret}`) {
         return new Response('Unauthorized', {
             status: 401,
         });
     }
-
-    console.log("Cron Authenticated")
+    console.log("CRON AUTHENTICATED=============================")
+    processScheduledPaymentsJob();
 
     return Response.json({ success: true });
 }
